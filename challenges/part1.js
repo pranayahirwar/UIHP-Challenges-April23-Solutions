de8bf696cd7dbbc32caf5f5a6442958d25a763c4
pranayahirwar <pranayahirwar.gmail.com>
2024-03-29 12:28:51 +0530

    Solved Part 1
    - Now we can store new char inputted in input element by user in
      post variable in JS and using it updating jsDiv element content

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,22 @@
 /** Write your code below */
+console.log("Script started running...");
+
+// VARS
+let post = "";
+
+let jsInput = document.querySelector("input");
+let jsDiv = document.querySelector("div");
+
+function handleInput() {
+    console.log("Running handleInput...");
+    
+    // Saving newly updated char in post var, to update jsDiv
+    post = jsInput.value;
+
+    // updating jsDiv with freshly updated post var.
+    jsDiv.textContent = post;
+}
+
+// Because of input type event, handleInput will be called each time, when user input
+// each character.
+jsInput.addEventListener("input", handleInput);
