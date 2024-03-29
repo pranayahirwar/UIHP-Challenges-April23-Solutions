faea823d4cf32cb0c6e43bac9ec825a0ddacc380
pranayahirwar <pranayahirwar.gmail.com>
2024-03-29 18:01:12 +0530

    Added feat which will update input and div element
    - Now input and dive element will be updated based on data myName
      variable.

--- challenges/part3.js
+++ challenges/part3.js
@@ -4,29 +4,34 @@ let isFocus = false;
 let jsInput;
 let jsDiv;
 
-function dataToView() {
-    jsInput.value = myName;
-    jsDiv.textContent = myName;
-}
-
-function handleInput() {
-    myName = jsInput.value;
-}
-
 function component() {
     // Line #1 & #2 if condition is used to focus our cursor again on newly rendred input element
     document.activeElement === jsInput ? (isFocus = true) : (isFocus = false); // Line #1
 
-    //your code here
+    /**
+     * Updating myName variable with value of jsInput.value before creating new jsInput element
+     *
+     * Below if condition mean, if jsInput is undefined then at this point we haven't created
+     * jsInput object, which contain HTML input element, because of which it won't have
+     * jsInput.value, So let's continue.
+     */
+    if (jsInput != undefined) {
+        myName = jsInput.value;
+    }
+
+    // Creating HTML Element in JS
     jsInput = document.createElement("input");
     jsDiv = document.createElement("div");
-    jsDiv.textContent = "...";
 
+    // Updating Data for input and div element.
+    jsDiv.textContent = (myName == "") ? "..." : myName;
+    jsInput.value = myName;
+
+    // Attaching HTML Elements in JS to DOM
     document.body.replaceChildren(jsInput, jsDiv);
 
     if (isFocus) jsInput.focus(); // Line #2
 }
 
+setInterval(component, 1000);
 component();
-jsInput.oninput = handleInput;
-setInterval(dataToView, 2000);
