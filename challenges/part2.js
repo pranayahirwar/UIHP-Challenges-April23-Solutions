81cce15e9d8299a490efa26f30cfc476387af148
pranayahirwar <pranayahirwar.gmail.com>
2024-03-29 15:36:24 +0530

    Solved Extension Challenge: Submit Button
    - Now user can click submit button and persist posts data until
      browser is refreshed

--- challenges/part2.js
+++ challenges/part2.js
@@ -7,16 +7,27 @@
 
 */
 let post = undefined;
+let didUserClickedSubmitButton = false;
 
 const jsInput = document.querySelector("input");
 const jsDiv = document.querySelector("div");
+const jsSavedDataDiv = document.querySelector(".saved-data");
+const jsButton = document.querySelector("button");
+
+function handleSubmitButton() {
+    // This function updates the data in JS and triggers the dataIsChangedUpdateUI() function to update the UI.
+    didUserClickedSubmitButton = true;
+    dataIsChangedUpdateUI();
+}
 
 function handleInput() {
+    // This function updates the data in JS and triggers the dataIsChangedUpdateUI() function to update the UI.
     post = jsInput.value;
     dataIsChangedUpdateUI();
 }
 
 function fnRemoveDefaultValueOfjsInput() {
+    // This function updates the data in JS and triggers the dataIsChangedUpdateUI() function to update the UI.
     post = "";
     dataIsChangedUpdateUI();
 }
@@ -25,18 +36,31 @@ function dataIsChangedUpdateUI() {
     // This function must contain all the code responsible for change in UI based on some
     // kind of data (like post variable)
 
+    if (didUserClickedSubmitButton == true) {
+        let newDiv = document.createElement("div");
+        let newParagraph = document.createElement("p");
+        newParagraph.textContent = post;
+
+        newDiv.appendChild(newParagraph);
+        jsSavedDataDiv.appendChild(newDiv);
+
+        // Resetting post to display default data
+        post = undefined;
+        didUserClickedSubmitButton = false;
+    }
+
     if (post == undefined) {
         jsInput.value = "Enter What's on your mind !!!";
     } else {
         jsInput.value = post;
         jsDiv.textContent = post;
     }
-
 }
 
 // Another way to add Event Handler.
 jsInput.oninput = handleInput;
 jsInput.onclick = fnRemoveDefaultValueOfjsInput;
+jsButton.onclick = handleSubmitButton;
 
 dataIsChangedUpdateUI();
 
